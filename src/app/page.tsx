4b1e7e500e28df992779f3b3f2b80a0497be8cd5
KevinShih-689 <kevin061215@gmail.com>
2024-12-27 11:07:52 +0800

    feat: Update package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,9 +42,8 @@ export default function Home () {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            "Read" our docs
+            Read our docs
           </a>
-
         </div>
       </main>
       <footer className={styles.footer}>
@@ -53,13 +52,7 @@ export default function Home () {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="https://nextjs.org/icons/file.svg" alt="File icon" width={16} height={16} />
           Learn
         </a>
         <a
@@ -67,13 +60,7 @@ export default function Home () {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="https://nextjs.org/icons/window.svg" alt="Window icon" width={16} height={16} />
           Examples
         </a>
         <a
@@ -81,13 +68,7 @@ export default function Home () {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="https://nextjs.org/icons/globe.svg" alt="Globe icon" width={16} height={16} />
           Go to nextjs.org →
         </a>
       </footer>
