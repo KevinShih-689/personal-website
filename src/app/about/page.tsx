3a51887b2fbde74859a4628cff5692c0473dca05
KevinShih-689 <kevin061215@gmail.com>
2025-03-03 15:55:55 +0800

    feat(About): Initial page #1

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import Typography from '@components/Typography/Typography';
+import styles from './styles.module.scss';
+
+export default function About () {
+  return (
+    <div>
+      <section id="home" className={styles.section}>
+        <Typography variant="h1">首頁</Typography>
+      </section>
+      <section id="about" className={styles.section}>
+        <Typography variant="h1">關於我</Typography>
+      </section>
+      <section id="collection" className={styles.section}>
+        <Typography variant="h1">作品集</Typography>
+      </section>
+    </div>
+  );
+}
--- src/stories/Header/Header.tsx
+++ src/stories/Header/Header.tsx
@@ -5,6 +5,9 @@ export default function Header () {
   return (
     <div className={styles.header}>
       <h1>This is header 標題</h1>
+      <a href="#home">首頁</a>
+      <a href="#about">關於我</a>
+      <a href="#collection">作品集</a>
       <ModeToggle />
     </div>
   );
