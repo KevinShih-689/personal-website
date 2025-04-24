4725b5474f32e3e01a9f920809f96b5b9a96d669
KevinShih-689 <kevin061215@gmail.com>
2025-04-24 09:37:58 +0800

    feat(styles): Replace color variables with text color variables in SCSS files for Header and ModeToggle components #1

--- src/stories/ModeToggle/ModeToggle.tsx
+++ src/stories/ModeToggle/ModeToggle.tsx
@@ -14,7 +14,7 @@ export default function ModeToggle () {
 
   const handleToggle = useCallback(() => setTheme(theme === 'dark' ? 'light' : 'dark'), [theme, setTheme]);
   const iconColor = useMemo(
-    () => (theme === 'dark' ? globalStyles.secondaryLightest : globalStyles.primaryLightest),
+    () => (theme === 'dark' ? globalStyles.textPrimaryDark : globalStyles.textPrimary),
     [theme]
   );
 
