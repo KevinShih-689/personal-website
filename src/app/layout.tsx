f49a3c38744c220bb5f2ff7c20083c45cc5b74c6
KevinShih-689 <kevin061215@gmail.com>
2025-04-24 16:04:49 +0800

    feat: Add react-scan package and integrate ReactScan component into the layout #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import { ThemeProvider } from 'next-themes';
 import Header from '@components/Header/Header';
+import ReactScan from '@components/ReactScan/ReactScan';
 import './globals.scss';
 
 const enFont = localFont({
@@ -28,6 +29,7 @@ export default function RootLayout ({
 }>) {
   return (
     <html lang="zh-TW" className={`${enFont.variable} ${chineseFont.variable}`} suppressHydrationWarning>
+      <ReactScan />
       <body>
         <ThemeProvider enableSystem disableTransitionOnChange>
           <Header />
