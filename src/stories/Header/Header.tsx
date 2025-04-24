fe48d36ff6319ea26e2c04e80b84292c3eca015f
KevinShih-689 <kevin061215@gmail.com>
2025-04-24 11:15:11 +0800

    feat(Header): Implement visibility toggle for header using IntersectionObserver; add sentinel element and transition effects in styles #1

--- src/stories/Header/Header.tsx
+++ src/stories/Header/Header.tsx
@@ -1,15 +1,37 @@
+'use client';
+
+import { useState, useRef, useEffect } from 'react';
 import ModeToggle from '@components/ModeToggle/ModeToggle';
 import styles from './Header.module.scss';
 
 export default function Header () {
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+  const sentinelRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      //TODO: Need to use throttle to prevent too many re-renders
+      ([entry]) => setIsHeaderVisible(entry.isIntersecting),
+      {
+        root: null,
+        threshold: 1.0,
+      }
+    );
+    if (sentinelRef.current) observer.observe(sentinelRef.current);
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <div className={styles.header}>
-      <a href="#home">資訊</a>
-      <a href="#about">能力</a>
-      <a href="#projects">專案</a>
-      <a href="#experience">經驗</a>
-      <a href="#contact">聯絡</a>
-      <ModeToggle />
-    </div>
+    <>
+      <div ref={sentinelRef} className={styles.sentinel} />
+      <div className={`${styles.header} ${isHeaderVisible ? '' : styles.hidden}`}>
+        <a href="#home">資訊</a>
+        <a href="#about">能力</a>
+        <a href="#projects">專案</a>
+        <a href="#experience">經驗</a>
+        <a href="#contact">聯絡</a>
+        <ModeToggle />
+      </div>
+    </>
   );
 }
