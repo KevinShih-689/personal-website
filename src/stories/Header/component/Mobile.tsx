c8eecd8a1ef11a5814e895ba1b69a269d6740260
KevinShih-689 <kevin061215@gmail.com>
2025-04-25 18:28:11 +0800

    feat(Header): Implement mobile header component with hamburger menu functionality and update desktop header styles for consistency #1

--- src/stories/Header/component/Mobile.tsx
+++ src/stories/Header/component/Mobile.tsx
@@ -1,3 +1,21 @@
+'use client';
+
+import { useState } from 'react';
+import styles from '../Header.module.scss';
+
 export default function Mobile () {
-  return <div>mobile header</div>;
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className={styles.mobile}>
+      <div
+        className={`${styles.hamburger} ${isActive ? styles['is-active'] : ''}`}
+        onClick={() => setIsActive(!isActive)}
+      >
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+      </div>
+    </div>
+  );
 }
