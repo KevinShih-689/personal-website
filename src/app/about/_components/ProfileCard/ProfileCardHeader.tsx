ca9e933841d0b55c8b9d8c88fd352c95ce05b8f9
KevinShih-689 <kevin061215@gmail.com>
2025-04-25 15:24:37 +0800

    feat(ProfileCard): Add loading state to ProfileCardHeader and implement loader styles for improved user experience #1

--- src/app/about/_components/ProfileCard/ProfileCardHeader.tsx
+++ src/app/about/_components/ProfileCard/ProfileCardHeader.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { useTheme } from 'next-themes';
 import IconButton from '@components/IconButton/IconButton';
 import DownloadIcon from '@icons/download.svg';
@@ -8,10 +8,16 @@ import globalStyles from '@app/variables.module.scss';
 import styles from './ProfileCard.module.scss';
 
 export default function ProfileCardHeader () {
+  const [mounted, setMounted] = useState(false);
   const { theme } = useTheme();
+
   const iconColor = useMemo(() => {
     return theme === 'dark' ? globalStyles.secondaryLightest : globalStyles.gray200;
   }, [theme]);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return <div className={styles.loader} />;
 
   return (
     <div className={styles.header}>
