30e061eac17ad581e47268f89a063f983e6e2c8d
KevinShih-689 <kevin061215@gmail.com>
2025-04-30 17:28:41 +0800

    refactor(Header): Simplify useMobileHeader hook by removing unnecessary refs and optimizing scroll event handling for improved performance #1

--- src/stories/Header/hooks/useMobileHeader.ts
+++ src/stories/Header/hooks/useMobileHeader.ts
@@ -1,10 +1,8 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import throttle from '@utils/throttle';
 
 export default function useMobileHeader () {
   const [isOpen, setIsOpen] = useState(false);
-  const lastScrollY = useRef(0);
-  const isOpenRef = useRef(isOpen);
 
   const scrollThrottle = throttle<boolean>((isVisible) => {
     setIsOpen(isVisible);
@@ -13,16 +11,7 @@ export default function useMobileHeader () {
   const handleSwitch = () => setIsOpen(!isOpen);
 
   useEffect(() => {
-    isOpenRef.current = isOpen;
-  }, [isOpen]);
-
-  useEffect(() => {
-    const handleScrollEvent = () => {
-      const currentScrollY = window.scrollY;
-      const isScrollingDown = currentScrollY > lastScrollY.current;
-      if (isScrollingDown && isOpenRef.current) scrollThrottle(false);
-      lastScrollY.current = currentScrollY;
-    };
+    const handleScrollEvent = () => scrollThrottle(false);
 
     window.addEventListener('scroll', handleScrollEvent);
     return () => window.removeEventListener('scroll', handleScrollEvent);
