d5fa7e982a1e95b7eb05464c8622a092f1a8c9c8
KevinShih-689 <kevin061215@gmail.com>
2025-04-30 09:49:23 +0800

    refactor(Header, Mobile): Simplify Header styles by removing theme properties for background color, enhance Hamburger component functionality with state management in Mobile component #1

--- src/stories/Header/component/Mobile.tsx
+++ src/stories/Header/component/Mobile.tsx
@@ -4,20 +4,17 @@ import { useState } from 'react';
 import styles from '../Header.module.scss';
 
 export default function Mobile () {
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className={styles.mobile}>
-      <Hamburger />
+      <Hamburger {...{ isActive, handleSwitch: () => setIsActive(!isActive) }} />
     </div>
   );
 }
 
-function Hamburger () {
-  const [isActive, setIsActive] = useState(false);
+function Hamburger ({ isActive, handleSwitch }: { isActive: boolean; handleSwitch: () => void }) {
   return (
-    <div
-      className={`${styles.hamburger} ${isActive ? styles['is-active'] : ''}`}
-      onClick={() => setIsActive(!isActive)}
-    >
+    <div className={`${styles.hamburger} ${isActive ? styles['is-active'] : ''}`} onClick={handleSwitch}>
       <span className={styles.line}></span>
       <span className={styles.line}></span>
       <span className={styles.line}></span>
