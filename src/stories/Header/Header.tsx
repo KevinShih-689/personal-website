d5fbcf678287b96053c8a3980727ccb2c9cb979d
KevinShih-689 <kevin061215@gmail.com>
2025-05-28 11:27:05 +0800

    style(eslintrc, Header): Clean up ESLint configuration for better readability and add TODO comment in Header component for future device type handling #1refactor(Header): Simplify useMobileHeader hook by removing unnecessary refs and optimizing scroll event handling for improved performance #1

--- src/stories/Header/Header.tsx
+++ src/stories/Header/Header.tsx
@@ -4,8 +4,10 @@ import useMediaQuery from '@lib/hooks/useMediaQuery';
 import Desktop from './component/Desktop';
 import Mobile from './component/Mobile';
 
-export default function Header () {
+export default function Header() {
+  //TODO: Use userAgent to get device type 'mobile', 'tablet', 'console', 'smarttv'??
   const device = useMediaQuery();
+
   if (!device) return null;
 
   return <>{device === 'mobile' ? <Mobile /> : <Desktop />}</>;
